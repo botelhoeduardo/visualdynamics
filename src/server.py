53d9cae87c6f83f054be0ddc3ecec481d71651bf
botelhoeduardo <eduardo.buganemi@gmail.com>
2018-04-20 01:50:56 -0400

    post method test

--- src/server.py
+++ src/server.py
@@ -1,9 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
+    if request.method == 'POST':
+        pass
     return render_template('index.html')
 
 if __name__ == '__main__':

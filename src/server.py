af9b93a8bfd9031a191596f029bb9c93ad8eed42
Eduardo <eduardo.buganemi@gmail.com>
2018-05-17 11:07:52 -0400

    getting from form

--- src/server.py
+++ src/server.py
@@ -1,12 +1,33 @@
 from flask import Flask, render_template, request
+import os
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
-        #testing
-        pass
+        selecao_arquivo = request.form['file']
+        pasta = os.path.dirname(selecao_arquivo)
+        arquivo = os.path.basename(selecao_arquivo)
+        (nome_arquivo, extensao) = arquivo.split('.')
+
+        campo_forca = request.form.get('campoforca')
+
+        modelo_agua = request.form.get('modeloagua')
+
+        tipo_caixa = request.form.get('tipocaixa')
+
+        distancia_caixa = request.form['distanciacaixa']
+
+        neutralizar_sistema = request.form['neutralize']
+
+        arquivo_gro = nome_arquivo + '.gro'
+        arquivo_top = nome_arquivo + '.top'
+        arquivo_box = nome_arquivo + '_box'
+        arquivo_ionizado = nome_arquivo + '_charged'
+        
+        
+
     return render_template('index.html')
 
 if __name__ == '__main__':

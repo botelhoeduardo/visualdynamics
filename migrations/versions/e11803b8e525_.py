715e142a9e91a17c13ef23ca66538ef44e5e6a1f
Eduardo <eduardo.buganemi@gmail.com>
2018-08-20 10:28:54 -0400

    trying to change directory

---
+++ migrations/versions/e11803b8e525_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: e11803b8e525
+Revises: 
+Create Date: 2018-07-31 10:46:03.570305
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'e11803b8e525'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
